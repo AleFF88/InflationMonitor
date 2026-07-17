6c3051025c06126caac65f3c25d713e87d618ea8
AleFF88 <1871847@gmail.com>
2026-07-17 19:16:36 +0300

    Cosmetic changes

--- InflationMonitor.Domain/Exceptions/Exceptions.cs
+++ InflationMonitor.Domain/Exceptions/Exceptions.cs
@@ -1,16 +1,11 @@
 ﻿namespace InflationMonitor.Domain.Exceptions {
-    public abstract class DomainException : Exception {
-        protected DomainException(string message) : base(message) { }
+    public abstract class DomainException(string message) : Exception(message) {
     }
 
-    public class InvalidHistoricalPeriodException : DomainException {
-        public InvalidHistoricalPeriodException(string message) : base($"Invalid historical period: {message}.") { }
+    public class InvalidHistoricalPeriodException(string message) : DomainException($"Invalid historical period: {message}.") {
     }
 
-    public class DomainArgumentOutOfRangeException : DomainException {
-        public string ParamName { get; } 
-        public DomainArgumentOutOfRangeException(string parameterName, string message) : base($"Invalid value for {parameterName}: {message}.") {
-            ParamName = parameterName;
-        }
+    public class DomainArgumentOutOfRangeException(string parameterName, string message) : DomainException($"Invalid value for {parameterName}: {message}.") {
+        public string ParamName { get; } = parameterName;
     }
 }
