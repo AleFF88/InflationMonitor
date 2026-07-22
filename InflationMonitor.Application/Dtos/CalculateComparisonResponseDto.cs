c6e63663b0f131204f155b3516007caca8ec7d24
AleFF88 <1871847@gmail.com>
2026-07-22 19:44:36 +0300

    Create DTOs

---
+++ InflationMonitor.Application/Dtos/CalculateComparisonResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace InflationMonitor.Application.Dtos {
+    public class CalculateComparisonResponseDto {
+        public string StartDate { get; set; } = string.Empty;
+        public string EndDate { get; set; } = string.Empty;
+        public decimal InitialAmount { get; set; }
+        public CashUsdComparisonSummaryDto Summary { get; set; } = new();
+    }
+}
---
+++ InflationMonitor.Application/Dtos/CashUsdComparisonSummaryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace InflationMonitor.Application.Dtos {
+    public class CashUsdComparisonSummaryDto {
+        public decimal CashGrivna { get; set; }
+        public decimal InflationEquivalent { get; set; }
+        public decimal UsdEquivalent { get; set; }
+    }
+}
