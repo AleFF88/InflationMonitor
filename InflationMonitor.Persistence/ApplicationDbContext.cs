34e01646ccebca5afc4528bcdae4330a2aed9556
AleFF88 <1871847@gmail.com>
2026-07-22 19:03:25 +0300

    Create DBContext

---
+++ InflationMonitor.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using InflationMonitor.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InflationMonitor.Persistence {
+    public class ApplicationDbContext : DbContext {
+        public DbSet<InflationRate> InflationRates => Set<InflationRate>();
+         public DbSet<ExchangeRate> ExchangeRates => Set<ExchangeRate>();
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
