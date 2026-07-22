cc85f5d63b847c76a1f9cf6a4577299d75f01cb8
AleFF88 <1871847@gmail.com>
2026-07-22 19:58:32 +0300

    Implement CalculateComparisonQuery for CORS

---
+++ InflationMonitor.Application/Queries/CalculateComparison/CalculateComparisonQuery.cs
@@ -0,0 +1,17 @@
+﻿using InflationMonitor.Application.Dtos;
+using MediatR;
+
+namespace InflationMonitor.Application.Queries.CalculateComparison {
+    public class CalculateComparisonQuery : IRequest<CalculateComparisonResponseDto> {
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Amount { get; set; }
+
+        public CalculateComparisonQuery(DateTime startDate, DateTime endDate, decimal amount) {
+            StartDate = startDate;
+            EndDate = endDate;
+            Amount = amount;
+        }
+    }
+}
