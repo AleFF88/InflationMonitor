d209eb82eab3c02473ce27c57ffdbd7bf26e277e
AleFF88 <1871847@gmail.com>
2026-07-22 19:03:01 +0300

    Create configs

---
+++ InflationMonitor.Persistence/Configurations/ExchangeRateConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using InflationMonitor.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InflationMonitor.Persistence.Configurations {
+    public class ExchangeRateConfiguration : IEntityTypeConfiguration<ExchangeRate> {
+        public void Configure(EntityTypeBuilder<ExchangeRate> builder) {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.CurrencyCode)
+                .HasMaxLength(3)
+                .IsRequired();
+
+            builder.Property(x => x.Year)
+                .IsRequired();
+
+            builder.Property(x => x.Month)
+                .IsRequired();
+
+            builder.Property(x => x.Rate)
+                .HasPrecision(18, 6)
+                .IsRequired();
+        }
+    }
+}
---
+++ InflationMonitor.Persistence/Configurations/InflationRateConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using InflationMonitor.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InflationMonitor.Persistence.Configurations {
+    public class InflationRateConfiguration : IEntityTypeConfiguration<InflationRate> {
+        public void Configure(EntityTypeBuilder<InflationRate> builder) {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Year)
+                .IsRequired();
+
+            builder.Property(x => x.Month)
+                .IsRequired();
+
+            builder.Property(x => x.Value)
+                .HasPrecision(18, 6)
+                .IsRequired();
+        }
+    }
+}
