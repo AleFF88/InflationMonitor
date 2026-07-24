373771a1094b9273e7a3722ada9e573dd03981ca
AleFF88 <1871847@gmail.com>
2026-07-24 16:41:09 +0300

    Create controller

---
+++ InflationMonitor.WebApi/Controllers/ComparisonController.cs
@@ -0,0 +1,30 @@
+﻿using InflationMonitor.Application.Queries.CalculateComparison;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InflationMonitor.WebApi.Controllers {
+    [ApiController]
+    [Route("api/calculator")]
+    public class ComparisonController : ControllerBase {
+        private readonly IMediator _mediator;
+
+        public ComparisonController(IMediator mediator) {
+            _mediator = mediator;
+        }
+
+        [HttpGet("compare")]
+        public async Task<IActionResult> Compare(
+            [FromQuery] DateTime startDate,
+            [FromQuery] DateTime endDate,
+            [FromQuery] decimal amount,
+            CancellationToken cancellationToken) {
+
+            // Encapsulates incoming query string parameters into a MediatR query object
+            var query = new CalculateComparisonQuery(startDate, endDate, amount);
+            // Sends the query through the MediatR pipeline to be processed by CalculateComparisonQueryHandler
+            var result = await _mediator.Send(query, cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
