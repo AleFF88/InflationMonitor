8e7b6ff0582afb28fa10fffa755d9252329064a3
AleFF88 <1871847@gmail.com>
2026-09-03 18:09:55 +0300

    Add euro data to the response and allow null returns

--- InflationMonitor.Application/Dtos/CashUsdComparisonSummaryDto.cs
+++ InflationMonitor.Application/Dtos/CashUsdComparisonSummaryDto.cs
@@ -1,7 +1,9 @@
 ﻿namespace InflationMonitor.Application.Dtos {
     public class CashUsdComparisonSummaryDto {
         public decimal CashGrivna { get; set; }
-        public decimal InflationEquivalent { get; set; }
-        public decimal UsdEquivalent { get; set; }
+        public decimal? InflationEquivalent { get; set; }
+        public decimal? UsdEquivalent { get; set; }
+        public decimal? EurEquivalent { get; set; }
+
     }
 }
