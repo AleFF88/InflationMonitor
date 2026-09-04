c65e6d8542b37f0a9f526ded3dcfc0226095aec4
AleFF88 <1871847@gmail.com>
2026-09-04 19:19:09 +0300

    Apply migration (indexes)

--- InflationMonitor.Persistence/Migrations/20260904161450_AddIndexes.Designer.cs
+++ InflationMonitor.Persistence/Migrations/20260904161450_AddIndexes.Designer.cs
@@ -2,6 +2,7 @@
 using InflationMonitor.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InflationMonitor.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260904161450_AddIndexes")]
+    partial class AddIndexes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.18");
---
+++ InflationMonitor.Persistence/Migrations/20260904161450_AddIndexes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InflationMonitor.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_InflationRates_Year_Month",
+                table: "InflationRates",
+                columns: new[] { "Year", "Month" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExchangeRates_CurrencyCode_Year_Month",
+                table: "ExchangeRates",
+                columns: new[] { "CurrencyCode", "Year", "Month" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_InflationRates_Year_Month",
+                table: "InflationRates");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ExchangeRates_CurrencyCode_Year_Month",
+                table: "ExchangeRates");
+        }
+    }
+}
