40dbb550aa7e86aaf52e7974c8f9e4310664921d
AleFF88 <1871847@gmail.com>
2026-09-08 20:48:00 +0300

    Add financial instrument strategy and factory interfaces

---
+++ InflationMonitor.Application/Common/Interfaces/IBatchFinancialInstrumentStrategy.cs
@@ -0,0 +1,12 @@
+﻿namespace InflationMonitor.Application.Common.Interfaces {
+    public interface IBatchFinancialInstrumentStrategy {
+        string CategoryKey { get; }
+
+        Task<Dictionary<string, decimal?>> CalculateEquivalentsAsync(
+            IEnumerable<string> instrumentCodes,
+            DateTime startDate, 
+            DateTime endDate, 
+            decimal amount, 
+            CancellationToken cancellationToken);
+    }
+}
---
+++ InflationMonitor.Application/Common/Interfaces/IFinancialInstrumentFactory.cs
@@ -0,0 +1,5 @@
+﻿namespace InflationMonitor.Application.Common.Interfaces {
+    public interface IFinancialInstrumentFactory {
+        IBatchFinancialInstrumentStrategy GetStrategy(string categoryKey);
+    }
+}
